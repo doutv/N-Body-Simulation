a60facb09cf6dff5a2c9ffb168df7fdc88894347
Yongjin Huang <1765941460@qq.com>
2021-11-14 10:30:26 +0800

    separate cmakelist

--- rsync_all_to_slurm.sh
+++ rsync_all_to_slurm.sh
@@ -1,4 +1,4 @@
 rsync -a ./ slurm:/pvfsmnt/119010115/a3 -v --exclude-from=exclude.list
-rsync -a ./gen_sbatch.sh slurm:/pvfsmnt/119010115/a3/ -v
+# rsync -a ./gen_sbatch.sh slurm:/pvfsmnt/119010115/a3/ -v
 ssh -t slurm "chmod +x /pvfsmnt/119010115/a3/gen_sbatch.sh"
 ssh slurm
